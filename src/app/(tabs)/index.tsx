fee5211b20dacaa5ab2295520459b930a60f7904
Makgraph <milienmakenson3@gmail.com>
2024-10-03 17:07:26 -0400

    implement feed

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,14 @@
-import { Text } from "react-native";
+import { FlatList } from "react-native";
+import posts from "@/assets/data/posts.json";
+import PostListItem from "@/src/components/PostListItem";
 
-export default function About() {
-  return <Text>Feed</Text>;
+export default function FeedScreen() {
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      contentContainerStyle={{ gap: 10 }}
+      showsVerticalScrollIndicator={false}
+    />
+  );
 }
