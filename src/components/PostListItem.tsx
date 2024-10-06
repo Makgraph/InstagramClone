34feea1d5ce59d9932b3664c0df83dc4d5b52a99
Makgraph <milienmakenson3@gmail.com>
2024-10-06 18:18:32 -0400

    cloudinary images

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,18 +1,57 @@
-import { Image, Text, View } from "react-native";
+import React from "react";
+import { Image, Text, View, Platform } from "react-native";
 import { Ionicons, Feather, AntDesign } from "@expo/vector-icons";
+import { AdvancedImage } from "cloudinary-react-native";
+import { cld } from "../lib/cloudinary";
+
+// Import required actions
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+
+// Applying transformations
+const applyTransformation = (image) => {
+  return image.resize(thumbnail().width(411).height(411)); // Resize for post image
+};
+
+const applyAvatarTransformation = (image) => {
+  return image.resize(
+    thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))
+  ); // Resize for avatar
+};
 
 export default function PostListItem({ post }) {
+  const myImage = applyTransformation(cld.image(post.image)); // Apply transformation
+  const imageUrl = myImage.toURL();
+
+  const myAvatar = applyAvatarTransformation(cld.image(post.user.avatar_url)); // Apply transformation
+  const avatarUrl = myAvatar.toURL();
+
   return (
     <View className="bg-white">
       {/* Header */}
       <View className="p-3 flex-row items-center gap-2">
-        <Image
-          source={{ uri: post.user.image_url }}
-          className="w-12 aspect-square rounded-full"
-        />
+        {Platform.OS === "ios" ? (
+          <AdvancedImage
+            cldImg={myAvatar}
+            className="w-12 aspect-square rounded-full"
+          />
+        ) : (
+          <Image
+            source={{ uri: avatarUrl }}
+            className="w-12 aspect-square rounded-full"
+          />
+        )}
         <Text className="font-semibold">{post.user.username}</Text>
       </View>
-      <Image source={{ uri: post.image_url }} className="w-full aspect-[4/3]" />
+
+      {/* Content */}
+      {Platform.OS === "ios" ? (
+        <AdvancedImage cldImg={myImage} className="w-full aspect-[4/3]" />
+      ) : (
+        <Image source={{ uri: imageUrl }} className="w-full aspect-[4/3]" />
+      )}
+
       {/* Icons */}
       <View className="flex-row justify-between gap-3 p-3">
         <View className="flex-row gap-3">
@@ -20,7 +59,6 @@ export default function PostListItem({ post }) {
           <Ionicons name="chatbubble-outline" size={20} />
           <Feather name="send" size={20} />
         </View>
-
         <Feather name="bookmark" size={20} />
       </View>
     </View>
